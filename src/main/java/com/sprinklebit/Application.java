8fa520d8af887d0aaf22bb5fa728cdd149929c28
shcherbininin <sasha.shcherbinin@gmail.com>
2018-03-01 22:50:18 +0200

    --

--- src/main/java/com/sprinklebit/Application.java
+++ src/main/java/com/sprinklebit/Application.java
@@ -3,6 +3,7 @@ package com.sprinklebit;
 import com.sprinklebit.input.InputData;
 import com.sprinklebit.input.pojo.Parameters;
 import com.sprinklebit.input.pojo.Ride;
+import javafx.util.Pair;
 
 import java.util.List;
 
@@ -17,12 +18,17 @@ class Application {
 
     private static final String EXAMPLE_A_PATH = "a_example.in";
 
+    List<Pair<Integer, Ride>> startToPointASteps = null;
+
     public static void main(String[] args) {
 
         InputData inputData = new InputData(EXAMPLE_A_PATH);
         Parameters parameters = inputData.getParameters();
         List<Ride> rides = inputData.getRides();
 
+        for (int i = 0; i < parameters.getVehicles(); i++) {
+            
+        }
 
         System.out.println(parameters);
 //        System.out.println(rides);
