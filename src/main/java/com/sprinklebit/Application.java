c7308ef5e5dfa59c287bde7ae64e14c80c1051ee
shcherbininin <sasha.shcherbinin@gmail.com>
2018-03-01 21:41:36 +0200

    First commit

---
+++ src/main/java/com/sprinklebit/Application.java
@@ -0,0 +1,15 @@
+package com.sprinklebit;
+
+/**
+ * Created with Android Studio.
+ * User: Sasha Shcherbinin
+ * Date: 3/1/18
+ * Time: 9:25 PM
+ */
+
+class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
