9ffb57329af7f900a720f97e39d762a9515e0216
Stepan Mikhaylov <stepanmikhaylov.sprinklebit@gmail.com>
2018-03-04 19:41:09 +0200

    file_txt

--- src/main/java/com/sprinklebit/stepa/ResultWriter.java
+++ src/main/java/com/sprinklebit/stepa/ResultWriter.java
@@ -1,5 +1,9 @@
 package com.sprinklebit.stepa;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class ResultWriter {
@@ -25,7 +29,43 @@ public class ResultWriter {
     }
 
     public void printResult() {
+        printToFile();
+//        printToLog();
+    }
 
+    private void printToFile() {
+        try {
+
+            File outputFile = new File("outResult.txt");
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile));
+
+            StringBuilder sb = new StringBuilder();
+
+            for(Map.Entry<Integer, List<Integer>> vehicle : completedRoutes.entrySet()) {
+                sb.append(vehicle.getKey());
+                sb.append(" ");
+
+                for (Integer ride : vehicle.getValue()) {
+                    sb.append(ride);
+                    sb.append(" ");
+                }
+
+                sb.append("\n");
+            }
+
+
+
+            bw.write(sb.toString());
+            bw.flush();
+            bw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void printToLog() {
         System.out.println("Writing data with logger");
 
         for(Map.Entry<Integer, List<Integer>> vehicle : completedRoutes.entrySet()) {
